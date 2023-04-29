d49735f2d8c9d08cfe26ea195ecaa0af94b4d861
tienbot1 <tien.ru@mail.ru>
2023-04-29 11:46:23 +0900

    [FIX] отображение карточек

--- dist/js/script.js
+++ dist/js/script.js
@@ -257,7 +257,17 @@ window.addEventListener('DOMContentLoaded', function () {
     }
     return await res.json();
   };
-  getResource('http://localhost:3000/menu').then(data => {
+
+  // getResource('http://localhost:3000/menu')
+  //     .then(data => {
+  //         data.forEach(({img, altimg, title, descr, price}) => {
+  //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render()
+  //         })
+  //     })
+
+  getResource('http://localhost:3000/menu') // getResource и отрисовка карточек
+  .then(data => createCard(data));
+  function createCard(data) {
     data.forEach(_ref => {
       let {
         img,
@@ -266,31 +276,22 @@ window.addEventListener('DOMContentLoaded', function () {
         descr,
         price
       } = _ref;
-      new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+      const element = document.createElement('div');
+      price = price * 27;
+      element.classList.add('menu__item');
+      element.innerHTML = `
+                    <img src=${img} alt=${altimg}>
+                    <h3 class="menu__item-subtitle">${title}</h3>
+                    <div class="menu__item-descr">${descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${price}</span> грн/день</div>
+                    </div>
+                `;
+      document.querySelector('.menu .container').append(element);
     });
-  });
-
-  // getResource('http://localhost:3000/menu') // getResource и отрисовка карточек
-  //     .then(data => createCard(data))
-  //     function createCard(data){
-  //         data.forEach(({img, altimg, title, descr, price}) => {
-  //             const element = document.createElement('div')
-  //             price = price*27;
-  //             element.classList.add('menu__item')
-  //             element.innerHTML = `
-  //                 <img src=${img} alt=${altimg}>
-  //                 <h3 class="menu__item-subtitle">${title}</h3>
-  //                 <div class="menu__item-descr">${descr}</div>
-  //                 <div class="menu__item-divider"></div>
-  //                 <div class="menu__item-price">
-  //                     <div class="menu__item-cost">Цена:</div>
-  //                     <div class="menu__item-total"><span>${price}</span> грн/день</div>
-  //                 </div>
-  //             `;
-
-  //             document.querySelector('.menu .container').append(element)
-  //         })
-  //     }
+  }
   //
   // axios.get('http://localhost:3000/menu') // получение данных через библиотеку axios (подключена в index.html)
   // .then(data => {
